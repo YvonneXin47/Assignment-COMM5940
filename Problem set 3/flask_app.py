9733814176c96ef93ad8414860de12c836612d10
YvonneXin47 <43102331+YvonneXin47@users.noreply.github.com>
2019-02-25 02:19:49 +0800

    add membership table

--- Problem set 3/flask_app.py
+++ Problem set 3/flask_app.py
@@ -31,4 +31,14 @@ def chart():
     for item in dataset:
         name_list.append(item.get('name'))
         point_list.append(item.get('point'))
-    return render_template('chart.html', entries = zip(name_list, point_list))
+    return render_template('chart.html', title='Chart Page', entries = zip(name_list, point_list))
+
+@app.route('/membertable')
+def membertable():
+    r = requests.get('https://api.airtable.com/v0/appY3TVmrawJmEE8Q/membership_points?api_key=keyx6ScOw6mCv5Vc4&sortField=_createdTime&sortDirection=desc')
+    dict = r.json()
+    dataset = []
+    for i in dict['records']:
+            dict = i['fields']
+            dataset.append(dict)
+    return render_template('member.html', entries=dataset, title='Member Page')
